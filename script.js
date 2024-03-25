5d3ce36c5fb11783672ce62063889c1f0f6ac3dd
Yahya Mawhoub <2232298@collegemv.qc.ca>
2024-03-25 18:15:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -88,11 +88,13 @@ document.getElementById('nextBtn').addEventListener('click', () => {
 });
 
 document.getElementById('playBtn').addEventListener('click', () => {
-    if (currentMediaSession) {
+    if(currentMediaSession){
         if (isPlaying) {
             currentMediaSession.pause(null, onMediaCommandSuccess, onError);
+            document.getElementById('playBtn').innerHTML = '<i class="fa-solid fa-pause"></i>'
         } else {
             currentMediaSession.play(null, onMediaCommandSuccess, onError);
+            document.getElementById('playBtn').innerHTML = '<i class="fa-solid fa-play"></i>'
         }
         isPlaying = !isPlaying;
     }
