d5e1ba1e0ddc6734c3153d889e963a389cf58694
QuocDungTran380 <kevintran380@gmail.com>
2024-03-25 18:13:45 -0400

    update

--- script.js
+++ script.js
@@ -42,6 +42,7 @@ themeToggle.addEventListener('click', function() {
 document.getElementById('connectButton').addEventListener('click', () => {
     initializeApiOnly();
     if (currentSession) {
+        document.getElementById("connectButton").style.color = "green";
         currentSession.setReceiverVolumeLevel(0.4, onMediaCommandSuccess, onError);
     }
 });
